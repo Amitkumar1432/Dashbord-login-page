75368c2f8d536ac74eff8ea276aaa77307949aed
amit kumar <99002497+Amitkumar1432@users.noreply.github.com>
2023-02-20 10:39:59 +0530

    Dashbord-login-page
    Create Login and Registration Form In Dashbord

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,93 @@
+import { Grid } from "@mui/material";
+
+const Contact = () => {
+  return (
+    <>
+      <Grid container justifyContent="center">
+        <Grid item sm={10}>
+          <h1>Contact Page</h1>
+          <hr />
+          <p>
+            <p>
+              Lorem ipsum dolor sit amet. Qui perspiciatis dolor sit possimus
+              dicta est dignissimos temporibus sed ipsa sapiente ad galisum
+              officiis. Est reiciendis nostrum 33 molestiae sapiente a deleniti
+              laborum id dolores recusandae est cupiditate aliquid et aspernatur
+              laboriosam et totam quia. Non itaque repellendus eum incidunt
+              excepturi vel quos nisi in necessitatibus enim rem excepturi
+              impedit. Qui recusandae modi cum laboriosam officiis et expedita
+              debitis et neque facere? Ea repudiandae internos ut ratione
+              voluptatum ut explicabo veritatis. Non ratione deleniti est
+              explicabo doloremque non nesciunt distinctio? Aut minus
+              necessitatibus id possimus officia a dolor sapiente sed quod
+              quaerat! Rem animi dignissimos sed aliquid facere cum quas quasi
+              in voluptate voluptatem sed autem Quis! Id minima harum et optio
+              itaque qui dolores illo qui voluptatem nihil vel voluptates
+              voluptas est omnis molestias. Ut deleniti deserunt sed incidunt
+              voluptatum qui natus animi. Est explicabo rerum qui omnis sint ut
+              voluptatum odit eos quaerat sequi rem internos placeat ab
+              doloremque eius. Qui dolor optio ut suscipit nihil eum adipisci
+              voluptas et quidem perferendis aut nemo odit. Ea architecto
+              consequatur ea sapiente dolorum vel adipisci necessitatibus vel
+              neque galisum qui pariatur aspernatur est tempore sunt. Et rerum
+              nemo ut enim omnis eos suscipit sint.{" "}
+            </p>
+            <p>
+              Qui quae facilis ut dolor placeat qui ratione reiciendis ut
+              perspiciatis labore ut consectetur omnis. Et ducimus nostrum eos
+              porro provident qui quam tempora ut voluptate temporibus aut
+              cumque consequatur et debitis expedita. Est quia modi rem veniam
+              dignissimos ea iusto voluptas. Et iste voluptatibus aut iure dolor
+              sed quod asperiores et voluptatibus cupiditate sit itaque pariatur
+              et odit vero quo iure enim. Sed voluptatem laboriosam vel
+              inventore repellat aut aliquid laborum ut accusamus delectus. Vel
+              repellat architecto qui sapiente omnis aut sequi impedit et autem
+              sapiente. Qui galisum asperiores et molestiae quae qui illo ullam
+              ut nobis voluptas ut commodi deleniti. Aut eius repudiandae qui
+              nemo possimus et officiis asperiores aut necessitatibus maiores.
+              Sed rerum enim et autem facere aut tempora assumenda sit placeat
+              provident aut dolorem cupiditate sed quia eveniet qui nemo quidem.
+              Aut dolor tenetur hic quasi quisquam ab necessitatibus
+              perspiciatis qui impedit galisum et voluptatibus quisquam. In
+              cupiditate delectus et nulla aspernatur eos minima reprehenderit
+              sit voluptatibus ipsam et ipsum pariatur id blanditiis maxime. Nam
+              asperiores asperiores eum sint quis qui similique pariatur cum
+              voluptates labore. Ea galisum iure est dolorum assumenda cum
+              libero quae. Aut neque saepe sit reiciendis accusantium est
+              voluptatem nesciunt.{" "}
+            </p>
+            <p>
+              Eos quia animi ut molestiae quidem qui dolorum reprehenderit in
+              laborum suscipit et exercitationem vero et consectetur cumque qui
+              ducimus dignissimos. Non eligendi iusto est illum culpa in
+              architecto vero aut velit ratione cum corrupti voluptatem. Eum
+              dolorem sint quo perspiciatis natus rem nostrum aliquid qui odit
+              neque et quidem quae nam inventore rerum ut fugit maxime. Ut earum
+              illo est eligendi tempora est quas consequatur! Et delectus
+              voluptas id quia odio aut odit rerum. Qui quisquam magnam eos sint
+              eaque qui consequuntur asperiores ea aliquam dolor quo sint
+              similique. 33 nihil rerum in impedit libero qui harum consequatur
+              ex excepturi illo est culpa repellendus cum reprehenderit libero
+              quo inventore aperiam. Vel saepe quibusdam sed quas incidunt vel
+              molestias quia quo ipsa corrupti. Ea facilis voluptatum est harum
+              impedit nam debitis galisum eos velit enim eos nesciunt maiores
+              eos iusto nihil. Sit odio commodi et illo sapiente vel minima
+              quidem eum accusamus dolor vel dolores minima? Aut quis voluptatum
+              et aperiam quia est nisi blanditiis ut voluptatem praesentium ut
+              similique tempore qui consequatur totam. Eos soluta voluptas qui
+              aspernatur aliquam sed odit unde est quasi eligendi est saepe
+              exercitationem et molestiae necessitatibus et ullam dolor? Qui
+              galisum exercitationem et nobis commodi et maiores beatae ea
+              similique quasi sed ipsa corrupti id consectetur neque ea illo
+              doloribus. Qui vitae fuga est architecto neque ea exercitationem
+              labore qui ipsa quis qui voluptatem magnam eos omnis odit eos
+              possimus temporibus.{" "}
+            </p>
+          </p>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Contact;
